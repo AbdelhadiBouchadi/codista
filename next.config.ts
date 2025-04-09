159248ddf7a411804db4089db6c7555dd5532283
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2025-04-10 00:18:11 +0300

    eslint and next.config update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,32 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+    ],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
